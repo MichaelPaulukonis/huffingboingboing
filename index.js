df62d1892fb2feef7b7741da648fd63fa64691db
MichaelPaulukonis <xraysmalevich@gmail.com>
2014-10-24 11:19:38 -0400

    strategy1 implemented for logging (no tweeting).

--- index.js
+++ index.js
@@ -36,15 +36,15 @@ var clean = function(text) {
 
 // ### Screen Scraping
 function getHeadlines() {
-    console.log('inside of getHeadlines()');
+    // console.log('inside of getHeadlines()');
     var headlines = [];
     var dfd = new _.Deferred();
     request(baseUrl, function (error, response, body) {
-	console.log('inside of request');
+	// console.log('inside of request');
 	if (!error && response.statusCode === 200) {
 	    var $ = cheerio.load(body);
 	    var heads = $('h1>a');
-	    console.log('head count: ' + heads.length);
+	    // console.log('head count: ' + heads.length);
 
 	    $('h1>a').each(function() {
 		var title = $(this).text();
@@ -60,7 +60,7 @@ function getHeadlines() {
 	    dfd.resolve(headlines);
 	}
 	else {
-	    console.log('getHeadlines.else');
+	    // console.log('getHeadlines.else');
 	    dfd.reject();
 	}
     });
@@ -88,6 +88,80 @@ function getHeadlines() {
 // get a topic from a different category of news and fill in the blank for "______ Wins a Grammy".)
 // If we're unable to find a headline where we can easily find/replace, we simply try again.
 
+
+var dumpInfo = function(headline) {
+
+    console.log('\n\nheadline: ' + headline);
+
+    // so. doesn't always work, if we don't have a named entity.
+    // fail: "Make you own geometrical papercraft mask"`
+    var p = nlp.pos(headline);
+    var tokens = p[0].tokens;
+
+    for (var i = 0; i < tokens.length; i++) {
+        var t = tokens[i];
+        console.log('text: ' + t.text + ' (' + t.pos.tag + ')');
+    }
+
+    var nn = getNNarray(headline);
+    console.log(nn.join(' -  '));
+};
+
+var stripWord = function(word) {
+
+    var removals = ['"', ':', '-', ','];
+
+    for (var r in removals) {
+	word = word.replace(r, '');
+    }
+
+    return word;
+};
+
+var getNNarray = function(headline) {
+
+    var nn = [];
+    var tokens = nlp.pos(headline)[0].tokens;
+
+    for (var i = 0; i < tokens.length; i++) {
+        var t = tokens[i];
+	if (t.pos.tag === 'NN') {
+	    nn.push(stripWord(t.text));
+	}
+    }
+
+    return nn;
+
+};
+
+// simple strategy - replace all the nouns in one sentence with the nouns from another
+// It's something.
+var strategy1 = function(h1, h2) {
+
+    var sent = h1;
+
+    var nn1 = getNNarray(h1);
+    var nn2 = getNNarray(h2);
+    var nouns1 = nn1;
+    var nouns2 = nn2;
+
+    // if (nn1.length > nn2.length) {
+    // 	sent = h2;
+    // 	nouns1 = nn2;
+    // 	nouns2 = nn1;
+    // }
+
+    var limit = ( nn1.length < nn2.length ? nn1.length : nn2.length);
+
+    for (var i = 0; i < limit; i++) {
+	sent = sent.replace(nouns1[i], nouns2[i]);
+    }
+
+    return sent;
+
+};
+
+
 // This won't work for BoingBoing, since there are no "Categories" where the category is in the headline
 // hrm.
 // just chop the two headlines into pieces?
@@ -95,31 +169,25 @@ function getHeadlines() {
 function tweet() {
 
     getHeadlines().then(function(headlines) {
-	console.log('returned from getHeadlines');
+
 	var h1 = pickRemove(headlines);
 	var h2 = pickRemove(headlines);
 
-	console.log('headline1: ' + h1.name);
+	// for (var i = 0; i < headlines.length; i++) {
+	//     // console.log(headlines[i]);
+	//     dumpInfo(headlines[i].name);
+	// }
 
-	// so. doesn't always work, if we don't have a named entity.
-	// fail: "Make you own geometrical papercraft mask"`
-        var p = nlp.pos(h1.name);
-        var tokens = p[0].tokens;
+	// still having trouble w/ quotes:
+	// h1: "Kitty help," a photo shared in the Boing Boing Flickr Pool
+	// h2:Denim maintenance thread
+	// replaced: "Kitty Denim a maintenance thread shared in the Boing Boing Flickr Pool
 
-        for (var i = 0; i < tokens.length; i++) {
-            var t = tokens[i];
-            console.log('text: ' + t.text + ' (' + t.pos.tag + ')');
-        }
 
-	console.log('\n\nheadline2: ' + h2.name);
+	console.log('\nh1: ' + h1.name + '\nh2:' + h2.name);
 
-        p = nlp.pos(h2.name);
-        tokens = p[0].tokens;
+	console.log("replaced: " + strategy1(h1.name, h2.name));
 
-        for (i = 0; i < tokens.length; i++) {
-            var t = tokens[i];
-            console.log('text: ' + t.text + ' (' + t.pos.tag + ')');
-        }
 
 	// so. this doesn't work. we will have to split apart using some other means.
 	// OR - only use those sentences that DO have a named-entity in them
